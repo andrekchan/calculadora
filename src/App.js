d21bd26376ee5fcf1febb0efc91ab9ca1f474ea8
Andre Kim Chan <andrekchan@usp.br>
2020-10-22 20:49:53 -0300

    a

--- src/App.js
+++ src/App.js
@@ -2,38 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Display value="1"/>
-      <Display value="2"/>
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      currentOperand: '0'
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <Display value={this,state.correntOperand}/>
+        {/* <Display value="2"/> */}
 
-      <Button text="0"/>
-      <Button text="1"/>
-      <Button text="2"/>
-      <Button text="3"/>
-      <Button text="4"/>
-      <Button text="5"/>
-      <Button text="6"/>
-      <Button text="7"/>
-      <Button text="8"/>
-      <Button text="9"/>
+        <Button text="0"/>
+        <Button text="1"/>
+        <Button text="2"/>
+        <Button text="3"/>
+        <Button text="4"/>
+        <Button text="5"/>
+        <Button text="6"/>
+        <Button text="7"/>
+        <Button text="8"/>
+        <Button text="9"/>
 
-      <Button text="+"/>
-      <Button text="-"/>
-      <Button text="/"/>
-      <Button text="*"/>
+        <Button text="+"/>
+        <Button text="-"/>
+        <Button text="/"/>
+        <Button text="*"/>
 
-      {/*<Keypad/>
-      <OperationButtons/>
-      <EqualButton/> */}
-    </div>
-  );
+        {/*<Keypad/>
+        <OperationButtons/>
+        <EqualButton/> */}
+      </div>
+    );
+  }
 }
 
 function Button(props) {
   return(
-  <button>{props.text}</button>
+    <button onClick={console.log('Clicado')}>{props.text}</button>
   )
 }
 
